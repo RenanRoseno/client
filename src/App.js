10842391822e1585dc32ac88684ce7f3787f91ee
Renan Roseno <renan.santos@mv.com.br>
2022-05-09 10:48:10 -0300

    Implemeting new components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 //import logo from './logo.svg';
 import React from "react";
 import "./App.css";
+import { AppBar, Typography, Grow, Grid, Container } from "@material-ui/core";
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
 
 const App = () => {
   return (
-    <div>
-      <marquee>
-        <h1>Hello World</h1>
-      </marquee>
-    </div>
+    <Container>
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Memories
+        </Typography>
+        <img src="/images/memories.png" alt="memories" heigth="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 // function App() {
