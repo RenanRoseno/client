36f378b75a7da8af9861d3e864bdf34bad23f66b
Renan Roseno <rosenorenan@gmail.com>
2022-05-23 10:37:56 -0300

    Fix like bug and add constants file

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,9 +1,10 @@
 import * as api from "../api";
+import { CREATE, DELETE, FETCH_ALL, LIKE, UPDATE } from "../constants/actionTypes";
 
 export const getPosts = () => async (dispatch) => {
   try {
     const { data } = await api.fetchPost();
-    const action = { type: "FETCH_ALL", payload: data };
+    const action = { type: FETCH_ALL, payload: data };
 
     dispatch(action);
   } catch (error) {
@@ -15,7 +16,7 @@ export const createPost = (post) => async (dispatch) => {
   try {
     const { data } = await api.createPost(post);
 
-    dispatch({ type: "CREATE", payload: data });
+    dispatch({ type: CREATE, payload: data });
   } catch (error) {
     console.log(error);
   }
@@ -25,7 +26,7 @@ export const updatePost = (id, post) => async (dispatch) => {
   try {
     const { data } = await api.updatePost(id, post);
 
-    dispatch({ type: "UPDATE", payload: data });
+    dispatch({ type: UPDATE, payload: data });
   } catch (error) {
     console.log(error.message);
   }
@@ -35,7 +36,7 @@ export const deletePost = (id) => async (dispatch) => {
   try {
     await api.deletePost(id);
 
-    dispatch({ type: "DELETE", payload: id });
+    dispatch({ type: DELETE, payload: id });
   } catch (error) {
     console.log(error);
   }
@@ -45,8 +46,8 @@ export const likePost = (id) => async (dispatch) => {
   try {
     const { data } = await api.likePost(id);
 
-    dispatch({ type: "LIKE", payload: data });
+    dispatch({ type: LIKE, payload: data });
   } catch (error) {
     console.log(error);
   }
-}
+};
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,19 +1,27 @@
+import {
+  CREATE,
+  DELETE,
+  FETCH_ALL,
+  LIKE,
+  UPDATE,
+} from "../constants/actionTypes";
+
 const reducer = (posts = [], action) => {
-  switch (action.type) {  
-    case "DELETE":
+  switch (action.type) {
+    case DELETE:
       return posts.filter((post) => post._id !== action.payload);
 
-    case "FETCH_ALL":
+    case FETCH_ALL:
       return action.payload;
 
-    case "CREATE":
+    case CREATE:
       return [...posts, action.payload];
 
-    case "LIKE":
+    case LIKE:
       return posts.map((post) =>
         post._id === action.payload._id ? action.payload : post
       );
-    case "UPDATE":
+    case UPDATE:
       return posts.map((post) =>
         post.id === action.payload._id ? action.payload : post
       );
