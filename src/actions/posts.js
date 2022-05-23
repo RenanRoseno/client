68e48eebe5ea526f39314b02ab59ed71df2bbb71
Renan Roseno <rosenorenan@gmail.com>
2022-05-23 10:01:16 -0300

    Fix update like view

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -45,7 +45,7 @@ export const likePost = (id) => async (dispatch) => {
   try {
     const { data } = await api.likePost(id);
 
-    dispatch({ type: "UPDATE", payload: data });
+    dispatch({ type: "LIKE", payload: data });
   } catch (error) {
     console.log(error);
   }
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -10,6 +10,9 @@ const reducer = (posts = [], action) => {
       return [...posts, action.payload];
 
     case "LIKE":
+      return posts.map((post) =>
+        post._id === action.payload._id ? action.payload : post
+      );
     case "UPDATE":
       return posts.map((post) =>
         post.id === action.payload._id ? action.payload : post
