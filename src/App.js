718c9608d1cc5d533d4411f70b80d87e5aeef000
Renan Roseno <rosenorenan@gmail.com>
2022-05-24 10:32:37 -0300

    Init navbar styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 //import logo from './logo.svg';
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import { AppBar, Typography, Grow, Grid, Container } from "@material-ui/core";
+import { Grow, Grid, Container } from "@material-ui/core";
 import { useDispatch } from "react-redux";
 import { getPosts } from "./actions/posts";
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
 import useStyles from "./styles";
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
   const [currentId, setCurrentId] = useState(null);
@@ -19,17 +20,7 @@ const App = () => {
 
   return (
     <Container>
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">
-          Memories
-        </Typography>
-        <img
-          className={classes.image}
-          src="/images/memories.png"
-          alt="memories"
-          height="60"
-        />
-      </AppBar>
+      <Navbar></Navbar>
       <Grow in>
         <Container>
           <Grid
