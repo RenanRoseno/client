561ae39891a5b2b6ba3be8420636faf89880fa66
Renan Roseno <rosenorenan@gmail.com>
2022-05-24 11:06:09 -0300

    Update package.json and add new navbar component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,21 @@
 //import logo from './logo.svg';
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
-import { Grow, Grid, Container } from "@material-ui/core";
-import { useDispatch } from "react-redux";
-import { getPosts } from "./actions/posts";
-import Posts from "./components/Posts/Posts";
-import Form from "./components/Form/Form";
-import useStyles from "./styles";
+import { Container } from "@material-ui/core";
 import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Auth from "./components/Auth/Auth";
 
 const App = () => {
-  const [currentId, setCurrentId] = useState(null);
-  const classes = useStyles();
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getPosts());
-  }, [currentId, dispatch]);
-
   return (
     <Container>
-      <Navbar></Navbar>
-      <Grow in>
-        <Container>
-          <Grid
-            className={classes.mainContainer}
-            container
-            justifyContent="space-between"
-            alignItems="stretch"
-            spacing={3}
-          >
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId} />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home}></Route>
+        <Route path="/auth" exact component={Auth}></Route>
+      </Switch>
+      <Home />
     </Container>
   );
 };
