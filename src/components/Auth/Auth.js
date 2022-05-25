c46dde89300d2f3689dc0c30badfb724539391d3
Renan Roseno <rosenorenan@gmail.com>
2022-05-25 08:59:17 -0300

    Adding new authentication component

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,5 +1,12 @@
 import React from "react";
-
+import {
+  Avatar,
+  Button,
+  Paper,
+  Grid,
+  Typhography,
+  Container,
+} from "@material-ui/core";
 const Auth = () => {
   const state = null;
   return <div>AUTH</div>;
