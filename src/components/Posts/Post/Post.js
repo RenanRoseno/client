065d19cf66a32e854fe5ac3239e4091dfbeeea09
Renan Roseno <rosenorenan@gmail.com>
2022-05-27 23:39:33 -0300

    Updating like logic

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -8,6 +8,7 @@ import {
   Typography,
 } from "@material-ui/core";
 import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+import ThumbUpAltOutlined from '@material-ui/icons/ThumbUpAltOutlined';
 import DeleteIcon from "@material-ui/icons/Delete";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import Edit from '@material-ui/icons/Edit';
@@ -19,6 +20,20 @@ import { deletePost, likePost } from "../../../actions/posts";
 const Post = ({ post, setCurrentId }) => {
   const classes = useStyles();
   const dispatch = useDispatch();
+  const user = JSON.parse(localStorage.getItem("profile"));
+
+  const Likes = () => {
+    if (post.likes.length > 0) {
+      return post.likes.find((like) => like === (user?.result?.googleId || user?.result?._id))
+        ? (
+          <><ThumbUpAltIcon fontSize="small" />&nbsp;{post.likes.length > 2 ? `You and ${post.likes.length - 1} others` : `${post.likes.length} like${post.likes.length > 1 ? 's' : ''}` }</>
+        ) : (
+          <><ThumbUpAltOutlined fontSize="small" />&nbsp;{post.likes.length} {post.likes.length === 1 ? 'Like' : 'Likes'}</>
+        );
+    }
+
+    return <><ThumbUpAltOutlined fontSize="small" />&nbsp;Like</>;
+  };
 
   return (
     <Card className={classes.card}>
@@ -62,12 +77,12 @@ const Post = ({ post, setCurrentId }) => {
         <Button
           size="small"
           color="primary"
+          disabled={!user?.result}
           onClick={() => {
             dispatch(likePost(post._id));
           }}
         >
-          <ThumbUpAltIcon fontSize="small" />
-          {post.likeCount}
+          <Likes />
         </Button>
         <Button
           size="small"
